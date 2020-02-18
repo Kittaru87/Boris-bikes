31b2de1c326802b7fa7e167bb93211ec9c71f2a6
Sophia Bell <Kittaru87@gmail.com>
2020-02-18 20:40:20 +0000

    describing class test and expecting class to respond to release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,19 @@
-require 'practice'
-# describe "describe DockingStation" do
-#   it 'has the name DockingStation' do
-#     expect(DockingStation).to eq DockingStation
-#   end
-  RSpec.describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
-    # expect(DockingStation).to respond_to(:release_bike)
+require 'docking-station'
+
+RSpec.describe DockingStation do
+  it 'has the name DockingStation' do
+    expect(DockingStation).to eq DockingStation
   end
+
+  it { is_expected.to respond_to(:release_bike) }
+
+end
+
+
+  # RSpec.describe DockingStation do
+  # it { is_expected.to respond_to(:release_bike) }
+  #   # expect(DockingStation).to respond_to(:release_bike)
+  # end
   # it 'working method' do
   #   expect(bike.working?).to eq true
   # end
