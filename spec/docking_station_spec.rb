7e0770dbd6691f705507f9bb929d71d96486ab07
Sophia Bell <Kittaru87@gmail.com>
2020-02-19 16:26:39 +0000

    more tests for docking a bike and seeing if there is a bike in the docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,29 @@
 require 'docking-station'
 
-RSpec.describe DockingStation do
+describe DockingStation do
   it 'has the name DockingStation' do
     expect(DockingStation).to eq DockingStation
   end
 
-  it { is_expected.to respond_to (:release_bike) }
+  it { is_expected.to respond_to(:release_bike) }
 
+  it 'gets a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  # it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  
+  it { is_expected.to respond_to(:bike) }
+  
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it 'checking a station for a bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+  end
 end
