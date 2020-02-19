9ab9aa24ed896ee0f42ff3ee3fffb08fc320ccce
Sophia Bell <Kittaru87@gmail.com>
2020-02-19 10:13:43 +0000

    updated

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -3,16 +3,5 @@ class DockingStation
     def release_bike
     
     end
-    
-    # docking_station = DockingStation.new
-    # bike = docking_station.release_bike
-    # bike.working?
-end
-
-class Bike
-
-    def working?
-    
-    end
 
 end
