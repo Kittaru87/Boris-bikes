688576b4a82785ac3f75f279e9b2d210410a30a0
Sophia Bell <Kittaru87@gmail.com>
2020-02-19 17:46:42 +0000

    exception rule for no bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,13 +7,19 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
-  it 'gets a working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  it 'releases a working bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.release_bike).to eq bike
   end
 
-  # it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  it 'throws up an error if there are no bikes' do
+    expect{subject.release_bike}.to raise_error "no bikes here"
+  end
   
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  
+
   it { is_expected.to respond_to(:bike) }
   
   it 'docks a bike' do
@@ -24,6 +30,11 @@ describe DockingStation do
   it 'checking a station for a bike' do
     bike = Bike.new
     subject.dock_bike(bike)
+    # expect {raise subject.bike}.to raise_error
     expect(subject.bike).to eq bike
+
   end
+
+
+
 end
