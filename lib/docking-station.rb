b0f6b85fdc4dea5c627d23ad3f6765a547d4dbdd
Sophia Bell <Kittaru87@gmail.com>
2020-02-19 16:25:57 +0000

    adding instance variable and attr_reader

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -1,7 +1,20 @@
 class DockingStation
+    
+    attr_reader :bike
+
+
+    def bike
+      @bike
+    end
 
     def release_bike
-    
+      bike = Bike.new
     end
+
+    def dock_bike(bike)
+      @bike = bike 
+    end
+
+    
 
 end
