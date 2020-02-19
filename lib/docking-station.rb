daa8b663790f3c89d286c20b3fd09e38cfca684a
Sophia Bell <Kittaru87@gmail.com>
2020-02-19 17:46:11 +0000

    exception added

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -2,19 +2,14 @@ class DockingStation
     
     attr_reader :bike
 
-
-    def bike
-      @bike
-    end
-
     def release_bike
-      bike = Bike.new
+      raise "no bikes here" unless @bike
+      @bike
     end
 
     def dock_bike(bike)
       @bike = bike 
     end
 
-    
 
 end
