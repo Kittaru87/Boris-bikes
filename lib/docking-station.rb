24f02e32e5118925994822f70391fe2f8c93e71f
Sophia Bell <Kittaru87@gmail.com>
2020-02-20 15:17:30 +0000

    added empty array, set max capacity, added initialize, raised exception for full docking station

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -1,15 +1,26 @@
 class DockingStation
     
-    attr_reader :bike
+    attr_reader :bikes
+    # sets default capacity of 20 when substantiating a new DockingStation
+    def initialize(capacity: 20)
+      @bikes = []
+      @capacity = capacity
+    end
 
     def release_bike
-      raise "no bikes here" unless @bike
-      @bike
+      raise "no bikes here" if @bikes.empty?
+      @bikes
     end
 
     def dock_bike(bike)
-      @bike = bike 
+      raise "full - no more bikes thanks" if @bikes.length == @capacity
+      @bikes << bike 
     end
 
+  private
+
+  def full?
+    @bikes.length >= @capacity ? true : false
+  end
 
 end
