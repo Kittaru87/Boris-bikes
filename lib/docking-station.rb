2f3bb8464b76d0e31231aeefc348ffdbe787d1c8
Sophia Bell <Kittaru87@gmail.com>
2020-02-20 16:06:29 +0000

    adding variable capacity for docking station, removed magic numbers

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -1,19 +1,21 @@
 class DockingStation
     
-    attr_reader :bikes
+    attr_reader :bikes, :capacity
     # sets default capacity of 20 when substantiating a new DockingStation
-    def initialize(capacity: 20)
+    DEFAULT_CAPACITY = 20
+    
+    def initialize(capacity=DEFAULT_CAPACITY)
       @bikes = []
       @capacity = capacity
     end
 
     def release_bike
-      raise "no bikes here" if @bikes.empty?
+      raise "no bikes here" if empty?
       @bikes
     end
 
     def dock_bike(bike)
-      raise "full - no more bikes thanks" if @bikes.length == @capacity
+      raise "full - no more bikes thanks" if full?
       @bikes << bike 
     end
 
@@ -23,4 +25,8 @@ class DockingStation
     @bikes.length >= @capacity ? true : false
   end
 
+  def empty?
+    @bikes.empty? ? true : false 
+  end
+  
 end
